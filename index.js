f650e7cd157d6898797404fba8fcf8793c80972c
Sandrine Tuyizere <sandrauyizere@gmail.com>
2020-09-26 16:34:53 +0200

    adding arrays and first func

---
+++ index.js
@@ -0,0 +1,7 @@
+var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
+var femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
+
+function processUserData(){
+    var day = parseInt(document.getElementById(day).value);
+    var month = parseInt(document.getElementById(month).value);
+}
